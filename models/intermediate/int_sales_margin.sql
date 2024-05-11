636b1d5bb999387b7e62aca017dd54e62f258538
Amanda Maurell Lobo Pereira <amanda.maurell@gmail.com>
2024-05-11 16:39:11 +0000

    calculated sales margin by calculating purchase_cost and margin. Joined 2 tables to do so.

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,9 @@
+with sales_margin as (select *, 
+cast(sales.quantity as int64) * cast(product.purchase_price as float64) as purchase_cost
+-- select tables with the name of the model generated before
+from {{ref("stg_raw__sales")}} as sales 
+inner join {{ref("stg_raw__product")}} as product
+on sales.products_id = product.products_id)
+select *,
+cast(revenue as float64) - purchase_cost as margin
+from sales_margin
